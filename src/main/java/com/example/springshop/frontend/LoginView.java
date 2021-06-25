09b2f7651291ab96619679d6329618c09e4a05bd
Evgeny <angst_eam@mail.ru>
2021-06-25 23:38:20 +1000

    навесил логи на конструктор и методы, хотя если честно, совсем не понятно, где и когда использование аспектов действительно нужно

---
+++ src/main/java/com/example/springshop/frontend/LoginView.java
@@ -0,0 +1,31 @@
+package com.example.springshop.frontend;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.*;
+
+@Route("login")
+@PageTitle("Login | Shop")
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+    private LoginForm login = new LoginForm();
+
+    public LoginView() {
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+        add(new H1("Geekbrains Shop"), login);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        if (event.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            login.setError(true);
+        }
+    }
+}
